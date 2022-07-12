034424d69156296cce41f7e307bbb23bdb66c0cf
Mamata Shrestha <74054922+smamata@users.noreply.github.com>
2022-07-12 09:32:39 +0545

    added weather forecast

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todays/business_logic/bloc/bloc/weather_bloc.dart';
 import 'package:todays/pages/dashboard.dart';
 import 'package:todays/pages/weather_page.dart';
+import 'package:todays/repo/weather_repo.dart';
 
 void main() {
   runApp(const Todays());
@@ -16,13 +19,20 @@ class Todays extends StatefulWidget {
 class _TodaysState extends State<Todays> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: Dashboard.id,
-      routes: {
-        Dashboard.id: (context) => Dashboard(),
-        Weather.id: (context) => Weather(),
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: ((context) =>
+                WeatherBloc(RealWeatherRepo())..add(GetWeather()))),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: Dashboard.id,
+        routes: {
+          Dashboard.id: (context) => const Dashboard(),
+          Weather.id: (context) => const Weather(),
+        },
+      ),
     );
   }
 }
--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todays/business_logic/bloc/bloc/weather_bloc.dart';
 import 'package:todays/pages/widgets/weather_box.dart';
 
 class Weather extends StatefulWidget {
@@ -17,11 +19,27 @@ class _WeatherState extends State<Weather> {
           title: Text("Weather"),
           backgroundColor: Color(0xff65c4fe),
         ),
-        body: ListView.builder(
-            itemCount: 5,
-            itemBuilder: (BuildContext context, index) {
-              return WeatherBox(
-                  name: "sss", max: "jnjn", min: "mnjnj", rain: "njnjnj");
-            }));
+        body: BlocBuilder<WeatherBloc, WeatherState>(
+          builder: (context, state) {
+            if (state is WeatherFailed) {
+              return Center(
+                child: Text(state.message.toString()),
+              );
+            } else if (state is WeatherLoaded) {
+              return ListView.builder(
+                  itemCount: state.model.weather!.length,
+                  itemBuilder: (BuildContext context, index) {
+                    var data = state.model.weather![index];
+                    return WeatherBox(
+                        name: data.place,
+                        max: data.max,
+                        min: data.min,
+                        rain: data.rain);
+                  });
+            } else {
+              return CircularProgressIndicator();
+            }
+          },
+        ));
   }
 }
